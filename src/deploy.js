6f40e85d8d6a370b794e8b570fa4dd22b015dee0
hitrick <a.khitrik@qvant.ru>
2023-10-16 11:40:14 +0300

    add homepage

--- src/deploy.js
+++ src/deploy.js
@@ -1,6 +1,6 @@
 const ghpages = require('gh-pages');
 
 ghpages.publish('build', {
-    branch: 'main',
+    branch: 'gh-pages',
     repo: 'https://github.com/hitrik/test-bit.git'
 }, console.log).then(console.log);
